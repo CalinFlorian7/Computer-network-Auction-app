c2aa71ab865af4f8502abf725d633bf6bde405d0
RacaruCalin7 <racarucalinflorian@grmail.com>
2024-03-28 12:07:53 +0200

    product constructor with name,starting and final price attributes created

---
+++ classes/user/product/Product.py
@@ -0,0 +1,9 @@
+
+class Product:
+    def __init__(self, name, startingPrice,finalPrice):
+        self.name = name
+        self.startingPrice = startingPrice
+        self.finalPrice = finalPrice
+    
+
+    
