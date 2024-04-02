6341ae8598a9a65e2d52601d06a59b41ac87f119
RacaruCalin7 <racarucalinflorian@grmail.com>
2024-04-02 13:41:45 +0300

    updating the price for a certain product

--- classes/user/UserRegistry.py
+++ classes/user/UserRegistry.py
@@ -13,8 +13,8 @@ class UserRegistry:
     def getUsers(self):
         return self.__users
     def addUser(self,user):
-        if user.get_name() not in self.__users:
-            self.__users[user.get_name()]=user
+        if user.getName() not in self.__users:
+            self.__users[user.getName()]=user
     def userNameExists(self,userName):
         if userName in self.__users:
             return True
@@ -25,28 +25,30 @@ class UserRegistry:
             self.__users[userName].addProduct(product)
         else:
             print(f"User {userName} does not exist")
-    def updateProductFinalPrice(self,productName,updatedPrice):
-        for userName in self.__users:
-            userCurent=self.__users[userName]
-            products=userCurent.getProducts()
-    
-            if productName in products:
-                if (products[productName].getStartingPrice())>= updatedPrice:
-                    print(f"Price {updatedPrice} is lower than the starting price {products[productName].getStartingPrice()}")
-                elif (products[productName].getFinalPrice())>= updatedPrice:
-                    print(f"Price {updatedPrice} is lower than the final price {products[productName].getFinalPrice()}")
-                else:
-
-                     products[productName].setFinalPrice(updatedPrice)
-            else:
-                print(f"Product {productName} does not exist for user {userName}")
             
+            
+    def updateProductFinalPrice(self, userName, productName, updatedPrice):
+                if userName in self.__users:
+                    userCurrent = self.__users[userName]
+                    products = userCurrent.getProducts()
+
+                    if productName in products:
+                        if products[productName].getStartingPrice() >= updatedPrice:
+                            return f"Price {updatedPrice} is lower than the starting price {products[productName].getStartingPrice()}"
+                        elif products[productName].getFinalPrice() >= updatedPrice:
+                            return f"Price {updatedPrice} is lower than the final price {products[productName].getFinalPrice()}"
+                        else:
+                            products[productName].setFinalPrice(updatedPrice)
+                    else:
+                        return f"Product {productName} does not exist for user {userName}"
+                else:
+                    return f"User {userName} does not exist"  
     def displayUsers(self):
         print("Users already added:")
         for userName in self.__users:
             userCurent=self.__users[userName]
-            print(userCurent.__str__())
-            for product in userCurent.getProducts():
-                print(product)
+            userCurent.displayUserProducts()
+            # for product in userCurent.getProducts():
+            #     print(product.__str__())
             
         #   print(f"User name{user.get_name()} Starting Price{user.get_startingPrice()} Final price {user.get_finalPrice()}")    
