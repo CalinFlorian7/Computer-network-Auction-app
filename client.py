049a46fc609a62a67b9eea91a8f51c78fd9d006d
RacaruCalin7 <racarucalinflorian@grmail.com>
2024-04-08 17:04:59 +0300

    crating a menu options for the user

--- client.py
+++ client.py
@@ -3,9 +3,16 @@ Client=classes.Client
 Endpoint=classes.Endpoint
 
 
+
 client=Client("127.0.0.1",1234)
 client.connect()
-client.insertUser(Endpoint.INSERTUSER.value)
+# client.insertUser(Endpoint.INSERTUSER.value)
+
+while True:
+    print("Write am option from below to continue:")
+    print("1. Add product")
+    print("2. Start auction")
+    chosenOption=input("Your choise option: ")
 
 
 # while True:
