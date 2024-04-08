7715933b15fea8d6613a40499dda321128aaea33
RacaruCalin7 <racarucalinflorian@grmail.com>
2024-04-08 16:49:50 +0300

    apelating the insertUser method

--- client.py
+++ client.py
@@ -5,11 +5,14 @@ Endpoint=classes.Endpoint
 
 client=Client("127.0.0.1",1234)
 client.connect()
+client.insertUser(Endpoint.INSERTUSER.value)
 
-while True:
-    name=input("Enter name: ")
-    client.insertUser(name,Endpoint.INSERTUSER.value)
-    response=client.receive_data()  
-    print("Received response: ",response)
+
+# while True:
+#     name=input("Enter name: ")
+#     client.insertUser(name,Endpoint.INSERTUSER.value)
+#     response=client.receive_data() 
+#     if response!=None: 
+#         print("Received response: ",response)
 
 client.close()
