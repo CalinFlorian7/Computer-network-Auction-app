6454117525422c430c6c61c7afe73d6e4ec8b371
RacaruCalin7 <racarucalinflorian@grmail.com>
2024-04-08 18:58:38 +0300

    testing the insertProduct on the client side

--- client.py
+++ client.py
@@ -6,13 +6,17 @@ Endpoint=classes.Endpoint
 
 client=Client("127.0.0.1",1234)
 client.connect()
-# client.insertUser(Endpoint.INSERTUSER.value)
+userName=""
+client.insertUser(Endpoint.INSERTUSER.value)
+print("you name for this auction: "+userName)
 
 while True:
     print("Write am option from below to continue:")
     print("1. Add product")
     print("2. Start auction")
     chosenOption=input("Your choise option: ")
+    if(chosenOption=="1"):
+        client.insertProduct(Endpoint.INSERTPRODUCT.value)
 
 
 # while True:
