f77609d001bf8b22a0696ed0e1d9dd04d78562e8
RacaruCalin7 <racarucalinflorian@grmail.com>
2024-04-08 16:50:39 +0300

    sending to the server the user name for registration

--- classes/clientServer/Client.py
+++ classes/clientServer/Client.py
@@ -1,16 +1,21 @@
 import socket
-
-
+import threading
 class Client:
     def __init__(self, server_ip, server_port):
         self.server_ip = server_ip
         self.server_port = server_port
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # self.receive_thread = threading.Thread(target=self.receive_data)
+        
+        
+      
 
     def connect(self):
         try:
             self.client_socket.connect((self.server_ip, self.server_port))
-            print("Connected to the server!")
+            # print("Connected to the server!")
+            print("Welcome to the auction!")
+            # self.receive_thread.start()
         except ConnectionRefusedError:
             print("Failed to connect to the server.")
 
@@ -21,11 +26,28 @@ class Client:
         except socket.error as e:
             print("Failed to send data:", str(e))
             
-    def insertUser(self,endpoint,userName):
+    def insertUser(self,endpoint):
         try:
-            self.client_socket.sendall((userName+"\n"+endpoint).encode())
+
+            print("To begin the auction, please enter your name: ")
+            while True:
+                userName = input()
+                if userName=="":
+                    print("Please enter a valid name!")
+                else:
+                    
+                    self.client_socket.sendall((endpoint+"\n"+userName).encode())
+                    print("Data sent successfully to the server!")
+                    response=self.receive_data()
+                    if(response=="error"):
+                        print("The user already exists, please enter another name:")
+                    else:
+                        print(response)
+                        break
+
             
-            print("The request to insert a user was sent successfully!")
+            
+           
         except socket.error as e:
             print("Failed to send data:", str(e))
 
