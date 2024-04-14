0750e9c9a85cee05a671fc52815a10b26865fdba
RacaruCalin7 <racarucalinflorian@grmail.com>
2024-04-14 23:28:28 +0300

    renaming the get methods

--- classes/auction/Auction.py
+++ classes/auction/Auction.py
@@ -4,7 +4,7 @@ Product=classes.Product
 class Auction:
         
         def __init__(self, userName,product):
-            self.__product = Product(product.get_name(),product.getStartingPrice())
+            self.__product = Product(product.getName(),product.getStartingPrice())
             self.__owner = userName
             self.__bids = {}
         
@@ -17,7 +17,7 @@ class Auction:
         def getProduct(self):
             return f"Product {self.__product.getName()} with starting price {self.__product.getStartingPrice()} and current price {self.__product.getFinalPrice()}"
         def bid(self,bidPrice,bidderName):
-            if bidPrice>self.__product.getFinalPrice():
+            if float(bidPrice)>self.__product.getFinalPrice():
                 self.__product.setFinalPrice(bidPrice)
                 self.__bids[bidPrice]=bidderName
                 return None
