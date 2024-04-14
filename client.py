ceb5f47e7b35a9f45819c871e53dc866c7f382f3
RacaruCalin7 <racarucalinflorian@grmail.com>
2024-04-14 23:25:47 +0300

    removing the menu from the client side

--- client.py
+++ client.py
@@ -1,26 +1,13 @@
 import classes
+import asyncio
 Client=classes.Client
 Endpoint=classes.Endpoint
+import threading
 
 
 
 client=Client("127.0.0.1",1234)
 client.connect()
-userName=""
-client.insertUser(Endpoint.INSERTUSER.value)
 
-while True:
-    print("----------------------------------------------------------------------")
-    print("Write am option from below to continue:")
-    print("1. Add product")
-    print("2. Start an auction")
-    print("3. Get products")
-    chosenOption=input("Your choise option: ")
-    if(chosenOption=="1"):
-        client.insertProduct(Endpoint.INSERTPRODUCT.value)
-    if(chosenOption=="2"):
-        client.startAuction(Endpoint.STARTAUCTION.value)
-   
-    
 
-client.close()
+
